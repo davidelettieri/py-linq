062ed5d74a7ae5193d917ea0b8419d496e93287d
davidelettieri <davidelettieri@users.noreply.github.com>
2019-07-06 09:47:48 +0200

    add take,skip,selectmany

--- py-linq/py_linq.py
+++ py-linq/py_linq.py
@@ -6,9 +6,27 @@ class Enumerable:
         return iter(self._data)
 
     def Where(self, predicate):
-        return WhereEnumerableIterator(self._data, predicate)
+        return _WhereEnumerableIterable(self, predicate)
 
-class WhereEnumerableIterator(Enumerable):
+    def Select(self, selector):
+        return _WhereSelectEnumerableIterable(self, _id, selector)
+
+    def SelectMany(self, selector):
+        return _SelectManyIterable(self, selector)
+
+    def Take(self, count):
+        return _TakeIterable(self, count)
+
+    def Skip(self, count):
+        return _SkipIterable(self, count)
+
+    def TakeWhile(self, predicate):
+        return _TakeWhileIterable(self, predicate)
+
+    def SkipWhile(self, predicate):
+        return _SkipWhileIterable(self, predicate)
+
+class _WhereEnumerableIterable(Enumerable):
     def __init__(self, data, predicate):
         self._data = data
         self._predicate = predicate
@@ -19,12 +37,12 @@ class WhereEnumerableIterator(Enumerable):
                 yield x
 
     def Select(self,selector):
-        return WhereSelectEnumerableIterator(self._data, self._predicate, selector)
+        return _WhereSelectEnumerableIterable(self._data, self._predicate, selector)
 
     def Where(self, predicate):
-        return WhereEnumerableIterator(self, combine_predicates(self._predicate,predicate))
+        return _WhereEnumerableIterable(self, _combine_predicates(self._predicate,predicate))
 
-class WhereSelectEnumerableIterator(Enumerable):
+class _WhereSelectEnumerableIterable(Enumerable):
     def __init__(self, data, predicate, selector):
         self._data = data
         self._predicate = predicate
@@ -36,13 +54,84 @@ class WhereSelectEnumerableIterator(Enumerable):
                 yield self._selector(x)
 
     def Select(self,selector):
-        return WhereSelectEnumerableIterator(self._data,self._predicate, combine_selectors(self._selector,selector))
+        return _WhereSelectEnumerableIterable(self._data,self._predicate, _combine_selectors(self._selector,selector))
 
     def Where(self, predicate):
-        return WhereEnumerableIterator(self, predicate)
+        return _WhereEnumerableIterable(self, predicate)
 
-def combine_predicates(predicate1, predicate2):
+class _SelectManyIterable(Enumerable):
+    def __init__(self, data, selector, resultSelector=None):
+        self._data = data
+        if resultSelector is None:
+            self._resultSelector = _id
+        else:
+            self._resultSelector = resultSelector
+
+        self._selector = selector
+
+    def __iter__(self):
+        for x in self._data:
+            for y in self._selector(x):
+                yield self._resultSelector(y)
+
+class _TakeIterable(Enumerable):
+    def __init__(self, data, count):
+        self._data = data
+        self._count = count
+
+    def __iter__(self):
+        if self._count == 0:
+            return
+
+        for x in self._data:
+            yield x
+            self._count -= 1
+            if self._count == 0:
+                return
+
+class _TakeWhileIterable(Enumerable):
+    def __init__(self, data, predicate):
+        self._data = data
+        self._predicate = predicate
+
+    def __iter__(self):
+        for x in self._data:
+            if self._predicate(x):
+                yield x
+            else:
+                return
+
+class _SkipIterable(Enumerable):
+    def __init__(self, data, count):
+        self._data = data
+        self._count = count
+
+    def __iter__(self):
+        for x in self._data:
+            if(self._count > 0):
+                self._count-=1
+                continue
+            yield x
+
+class _SkipWhileIterable(Enumerable):
+    def __init__(self, data, predicate):
+        self._data = data
+        self._predicate = predicate
+
+    def __iter__(self):
+        skip = True
+        for x in self._data:
+            if skip and self._predicate(x):
+                continue
+            else:
+                skip = False
+                yield x
+
+def _combine_predicates(predicate1, predicate2):
     return lambda x : predicate1(x) and predicate2(x)
 
-def combine_selectors(selector1,selector2):
+def _combine_selectors(selector1,selector2):
     return lambda x : selector2(selector1(x))
+
+def _id(x):
+    return x
--- py-linq/py_linq_test.py
+++ py-linq/py_linq_test.py
@@ -17,5 +17,90 @@ class Test_py_linq_test(unittest.TestCase):
         source = [i for i in it]
         self.assertCountEqual(source,[6,8,10])
 
+    def test_where_after_select(self):
+        class Test:
+            def __init__(self, data):
+                self.Data = data
+            def __eq__(self, other):
+                return self.Data == other.Data
+
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).Select(lambda x : Test(x)).Where(lambda x : x.Data >= 10)
+        source = [i for i in it]
+        self.assertEqual(len(source),1)
+        self.assertEqual(source[0], Test(10))
+
+    def test_take_1(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).Take(1)
+        source = [i for i in it]
+        self.assertEqual(len(source),1)
+        self.assertEqual(source[0], 1)
+
+    def test_take_2(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).Take(2)
+        source = [i for i in it]
+        self.assertEqual(len(source),2)
+        self.assertEqual(source[0], 1)
+        self.assertEqual(source[1], 2)
+
+    def test_skip_1(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).Skip(1)
+        source = [i for i in it]
+        self.assertEqual(len(source),9)
+        self.assertEqual(source[0], 2)
+
+    def test_skip_1_take_2(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).Skip(1).Take(2)
+        source = [i for i in it]
+        self.assertEqual(len(source),2)
+        self.assertEqual(source[0], 2)
+        self.assertEqual(source[1], 3)
+
+    def test_take_while(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).TakeWhile(lambda x: x < 5)
+        source = [i for i in it]
+        self.assertEqual(len(source),4)
+        self.assertEqual(source[0], 1)
+        self.assertEqual(source[1], 2)
+        self.assertEqual(source[2], 3)
+        self.assertEqual(source[3], 4)
+
+    def test_skip_while(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).SkipWhile(lambda x: x <= 5)
+        source = [i for i in it]
+        self.assertEqual(len(source),5)
+        self.assertEqual(source[0], 6)
+        self.assertEqual(source[1], 7)
+        self.assertEqual(source[2], 8)
+        self.assertEqual(source[3], 9)
+        self.assertEqual(source[4], 10)
+
+    def test_skipwhile_takewhile(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).SkipWhile(lambda x: x <= 5).TakeWhile(lambda x: x < 9)
+        source = [i for i in it]
+        self.assertEqual(len(source),3)
+        self.assertEqual(source[0], 6)
+        self.assertEqual(source[1], 7)
+        self.assertEqual(source[2], 8)
+
+    def test_selectmany(self):
+        it = Enumerable([1,2,3,4,5,6,7,8,9,10]).SelectMany(lambda x: range(x))
+        source = [i for i in it]
+        self.assertEqual(len(source),55)
+
+
+    def test_where_doesnt_change_source(self):
+        start = Enumerable([1,2,3,4,5,6,7,8,9,10])
+        a = start.Where(lambda x: x > 5)
+        b = start.Where(lambda x: x <= 5)
+
+        start_source = [i for i in start]
+        a_source = [i for i in a]
+        b_source = [i for i in b]
+
+        self.assertEqual(len(start_source),10)
+        self.assertEqual(len(a_source),5)
+        self.assertEqual(len(b_source),5)
+
+
 if __name__ == '__main__':
     unittest.main()
